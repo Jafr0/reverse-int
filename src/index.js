1bfc642c6266923a10ecf79d8cb583d4efd2dd94
Jafr0 <zhek42@yandex.ru>
2022-01-10 21:00:12 +0300

    fix: complete with minus

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let arr = n.toString().split('')
+	if (arr[0] == '-') {
+		let arrm = arr.shift()
+		let revarr = [arr.reverse().join('') / 1];
+		return revarr
+	}
+	let revarr = [arr.reverse().join('') / 1]
+	return revarr
 }
